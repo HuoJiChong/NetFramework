657a3a9a0ade2696dfe53524f39c62ec6e8f50e5
Machals <909068353@qq.com>
2019-08-20 20:32:58 +0800

    json请求测试

--- app/src/main/java/com/derek/netframework/MainActivity.java
+++ app/src/main/java/com/derek/netframework/MainActivity.java
@@ -2,6 +2,10 @@ package com.derek.netframework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.VelocityTracker;
+import android.view.View;
+
+import com.derek.velly.Velly;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        for (int i = 0;i<10;i++){
+
+        }
     }
 }
